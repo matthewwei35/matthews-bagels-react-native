c584ff9089db40319120e3d2453dfecadcb0a36b
Matthew Wei <matthewwei35@gmail.com>
2022-03-04 07:18:27 -0800

    style(by-breed): center content

--- Item.js
+++ Item.js
@@ -1,17 +1,29 @@
 import { Text, View, Image, StyleSheet } from 'react-native';
 
 const Item = ({ image, title }) => (
-  <View>
+  <View style={styles.container}>
     <Image style={styles.image} source={image} />
-    <Text>{title}</Text>
+    <Text style={styles.title}>{title}</Text>
   </View>
 );
 
 const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey'
+  },
   image: {
     width: '100%',
     height: 300,
   },
+  title: {
+    alignSelf: 'center',
+    fontSize: 16,
+    paddingTop: 4,
+  }
 });
 
 export default Item;
